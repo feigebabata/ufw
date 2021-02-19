127ddbe9f5a4b2f11adfbff0d83f9d542001e41c
feigebabata <feigebabata@outlook.com>
2021-02-19 11:14:52 +0800

    1.添加测试

---
+++ Runtime/FGUFW.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FGTest
+{
+    public class Test
+    {
+        public void Log()
+        {
+            Debug.Log("Test.Log******");
+        }
+    }
+}
